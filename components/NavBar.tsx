75dd490d3099874a8bfd8817ff86a1f7147aef05
Frankz <leninalarcon7@gmail.com>
2022-09-28 22:32:31 -0500

    initial commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <header className="h-12 bg-purple-600 flex justify-center items-center">
+      <div className="mr-32"><Image  width={30} height={30} src="/img/logo.png" alt="Logo Club de Software EPN" /></div>
+      <h1 className="text-2xl text-center text-white font-bold">Generador de Horarios</h1>
+    </header>
+  )
+}
---
+++ pages/schedule.tsx
@@ -0,0 +1,6 @@
+
+export default function schedule() {
+  return (
+    <div>schedule</div>
+  )
+}
